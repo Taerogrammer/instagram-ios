c26622b557ce4b0d593bc62af13a7b416c46ab7a
Taerogrammer <hyung54321@naver.com>
2023-01-28 15:45:09 +0900

    commit 연동 확인

---
+++ instagram_ios/instagram_ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  instagram_ios
+//
+//  Created by 김태형 on 2023/01/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
