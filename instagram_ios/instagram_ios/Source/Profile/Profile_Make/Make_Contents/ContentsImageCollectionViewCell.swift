6ff7f85cb81d8dfcb903a883da6c60835cc57efc
Taerogrammer <hyung54321@naver.com>
2023-02-04 04:10:15 +0900

    - 게시글 이미지 띄우기

---
+++ instagram_ios/instagram_ios/Source/Profile/Profile_Make/Make_Contents/ContentsImageCollectionViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  ContentsImageCollectionViewCell.swift
+//  instagram_ios
+//
+//  Created by 김태형 on 2023/02/04.
+//
+
+import UIKit
+
+class ContentsImageCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var deleteBtnCell: UIButton!
+    @IBOutlet weak var imageCell: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    
+}
